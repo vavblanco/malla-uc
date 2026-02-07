c4229c31ce75f94019edbf374665570f94d9c7ed
vicho <vicentevillalobosblanco@gmail.com>
2026-02-06 21:42:07 -0300

    Update bio.ts

--- src/data/carreras/cc/bio.ts
+++ src/data/carreras/cc/bio.ts
@@ -1,8 +1,8 @@
 import { Carrera } from '@/types/curriculum';
 
-export const bq: Carrera = {
-  codigo: 'BQ',
-  nombre: 'Bioquímica',
+export const bio: Carrera = {
+  codigo: 'BIO',
+  nombre: 'Biología (Malla Nueva)',
   campus: 'cc',
   color: '#4CAF50',
   
